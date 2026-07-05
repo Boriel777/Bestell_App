5b116c9bbe8692c8bb322f3c523b2df10032b9e2
Dalibor Durbas <office@boriel-designs.com>
2026-07-05 23:34:01 +0200

    Refactor zuAscii function and update getMenuTemplate to ensure consistent casing and formatting

--- scripts/templates.js
+++ scripts/templates.js
@@ -1,22 +1,24 @@
 function zuAscii(text) {
     return text
-        .replace(/ä/g, "ae")
+        .toLowerCase()
+        .replace(/ä/g, "ae")    
         .replace(/ö/g, "oe")
         .replace(/ü/g, "ue")
         .replace(/ß/g, "ss")
-        .replace(/Ä/g, "Ae")
-        .replace(/Ö/g, "Oe")
-        .replace(/Ü/g, "Ue");
-};
+        .replace(/\s+/g, "-");
+}
+
+
 
 function getMenuTemplate(category, meal) {
     let mealsHTML = "";
+    let id = zuAscii(category);
     for (let meal_i = 0; meal_i < meal.length; meal_i++) {
         mealsHTML += getMealTemplate(meal[meal_i]);
     }
-    let id = zuAscii(category.toLowerCase());
+    
     return `
-    <section id="cat-${category}">
+    <section id="cat-${id}">
         <h2 id="title-${id}">${category}</h2>
         <div>${mealsHTML}</div>
     </section>
@@ -28,7 +30,7 @@ function getMealTemplate(meal) {
     <div class="meal_wrapper" id="${meal.id}">
         <h3 class="meal_name">${meal.meal_name}</h3>
         <p><span>Zutaten:</span> ${meal.ingredients}</p>
-        <p><span>Preis:</span> € ${meal.price}</p>
+        <p><span>Preis:</span> € ${meal.price.toFixed(2)}</p>
     </div>
     `;
 };
